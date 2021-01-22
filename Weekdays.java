8374fd3a609e51e0e651fd090a770c129018a547
Christian Ofori-Achiaw <christianoforiachiaw@gmail.com>
2021-01-22 16:54:07 +0000

    Create Weekdays.java

---
+++ Weekdays.java
@@ -0,0 +1,19 @@
+package chris;
+
+import java.util.Date;
+
+
+public class Weekdays {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Sunday");
+        System.out.println("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.println("Friday");
+        System.out.println("Saturday");
+        }
+    }
+    
+}
