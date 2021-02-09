c169581421c1d4d376bb67d3b611e046fe5567e1
Christian Ofori-Achiaw <christianoforiachiaw@gmail.com>
2021-02-09 16:04:38 +0000

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,12 @@
+public class EvenNumbers {
+   public static void main(String args[]) {
+	  int n = 100;  
+	  int i = 1;
+	  while(i <= n) {
+	    if (i % 2 == 0) {
+	      System.out.print(i + " ");
+		  }
+	    i++;
+	  }
+  }
+}
