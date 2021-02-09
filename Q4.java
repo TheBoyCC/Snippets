df93f8ec927b1179bd456b2b5c0591358cfb5928
Christian Ofori-Achiaw <christianoforiachiaw@gmail.com>
2021-02-09 16:25:06 +0000

    Create Q4.java

---
+++ Q4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BooleanExpression 
+{
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+	      System.out.print("Enter number:");        
+	    
+	      int num = s.nextInt();
+        
+        if(num > 1 && num < 100 || num < 0) 
+        {
+            System.out.print("True");
+        }
+    }
+}
