c07a2e4d7b6a2d52df08ee20585efe77002c1ec9
Christian Ofori-Achiaw <christianoforiachiaw@gmail.com>
2021-02-09 16:39:01 +0000

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,15 @@
+public class IfElseStatement 
+{
+    public static void main(String[] args) 
+    {     
+	    int x = 3;
+	    int y = 3;
+        
+        if(x + 3 == 6) {
+            System.out.print(y = 1);
+        }
+        else {
+            System.out.print(y += 1);
+        }
+    }
+}
