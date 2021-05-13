7c9c8705f7a54860ff93fb1d9bd491cfc512c34f
Christian Ofori-Achiaw <christianoforiachiaw@gmail.com>
2021-05-13 15:33:49 +0000

    Create insert_actions_accTypes.sql

---
+++ insert_actions_accTypes.sql
@@ -0,0 +1,215 @@
+USE [CCS_DB_UAT]
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (0, N'CURRENT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (1, N'ORDINARY LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (2, N'MORTGAGE LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (3, N'STAFF CURRENT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (4, N'BUS SOLUTN LOAN FIXED', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (5, N'G''TEED SCHEME LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (6, N'SSI TRADING', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (7, N'FGN BOOKING', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (8, N'CALL DEPOSIT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (9, N'CUSTOMER FOREIGN CURRENCY', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (10, N'FIXED DEPOSIT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (12, N'NON REVALUATED SUSUPENSE', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (13, N'FWDS AND SWAPS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (14, N'MEMORANDUM', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (16, N'PROFIT AND LOSS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (17, N'EX-STAFF (RETIREES) C/A', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (18, N'EX-STAFF ORDINARY', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (19, N'EX-STAFF SAVINGS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (20, N'STAFF SAVINGS SCHEME', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (21, N'ABSA DIRECT B VARY', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (22, N'ULTIMATE ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (23, N'ABSA DIRECT B PRSNL LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (24, N'SPECIAL CALL ACCOUNTS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (25, N'ABSA JUNIOR SAVINGS A/C', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (26, N'MEDIUM BUSINESS C/A', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (27, N'MEDIUM BUSINESS CALL A/C', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (28, N'BB UNSECURE LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (29, N'CORPORATE TERM LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (30, N'CORPORATE VAF ASSETS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (31, N'TRADE LOAN GRR', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (32, N'SMALL BUSINESS CALL A/C', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (33, N'LOACL BUSINESS CALL', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (34, N'BOND GUA AND IND (BGI)', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (35, N'LOAN / OD / DEF DEB CARDS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (36, N'EX-STAFF-SECURED LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (37, N'TRADE LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (38, N'EXECUTIVE LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (39, N'EX-STAFF-UNSECURED LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (40, N'CORPORATE CURRENT ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (41, N'L/C CASH MARGIN ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (42, N'CUSTODY ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (43, N'CORPORATE SAVINGS A/C', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (44, N'ABSA ALLIANCE', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (45, N'SCHEME LTO', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (46, N'DIRECT B LTO', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (47, N'EX-STAFF PERSONAL VARI', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (48, N'EX-STAFF HOUSING VARI', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (49, N'EX-STAFF AUTO VARIABLE', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (50, N'PRESTIGE ULTIMATE ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (51, N'STAFF SAVINGS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (52, N'ABSA DIRECT A PRSNL LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (53, N'VSLA DWETIRI CURRENT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (54, N'CASH SECURITY', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (55, N'BB CASH MARGIN ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (56, N'CORPORATE VAF', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (58, N'RETAIL VAF', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (60, N'IGNITION STUDENT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (61, N'BUSINESS SOLUTION LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (62, N'COMMUNITY ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (63, N'SCHEME LOAN VARIABLE RATE', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (64, N'LB MASS CURRENT ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (65, N'BUSS SOLN SILVER CU ACC', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (66, N'LOCAL BUSS BONUS SAVINGS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (67, N'NON G''TEED SCHEME LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (68, N'BUSINESS SOLN GOLD CURR', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (69, N'BUSINESS SAVINGS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (70, N'BANK ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (71, N'BONUS SAVINGS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (72, N'INSTANT SAVINGS ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (73, N'PRESTIGE CURRENT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (74, N'TARGET SAVE', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (75, N'PRESTIGE PLUS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (76, N'HIGH INTEREST BONUS SAV', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (77, N'PRESTIGE BONUS SAVINGS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (78, N'PRESTIGE LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (79, N'HIGH VALUE CURRENT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (80, N'POTENTIAL LOSS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (81, N'CONTROL ACCOUNTS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (82, N'FINANCIAL ACCOUNTS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (83, N'NOSTRO ACCOUNTS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (84, N'LIABILITY ACCOUNTS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (85, N'HEAD OFFICE', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (86, N'RECONCILIABLE', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (87, N'PREMIER BONUS SAVINGS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (88, N'PREMIER LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (89, N'PREMIER CURRENT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (90, N'PREMIER HIGH INT BUNUS SA', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (91, N'NGO ACCOUNT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (92, N'STAFF EQUITY RELEASE LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (93, N'ABSA ABA PA CURRENT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (94, N'ABSA ABA PA SAVINGS', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (95, N'STAFF CAR LOAN A/C', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (96, N'SNR.MGTMT CAR LOAN BENEFIT', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (97, N'STAFF STUDY LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (98, N'STAFF PERSONAL LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+INSERT [dbo].[AccountTypes] ([AccountType], [TypeName], [TypeStatus], [DateModified]) VALUES (99, N'STAFF HOUSING LOAN', 1, CAST(N'2020-02-11 10:43:00.900' AS DateTime))
+GO
+SET IDENTITY_INSERT [dbo].[Actions] ON 
+
+GO
+INSERT [dbo].[Actions] ([ActionID], [ActionName], [ActionStatus], [DateModified]) VALUES (2, N'Unpay Cheque', 1, CAST(N'2020-02-17 11:38:38.290' AS DateTime))
+GO
+INSERT [dbo].[Actions] ([ActionID], [ActionName], [ActionStatus], [DateModified]) VALUES (3, N'Simple Demand', 1, CAST(N'2020-02-17 11:38:38.290' AS DateTime))
+GO
+INSERT [dbo].[Actions] ([ActionID], [ActionName], [ActionStatus], [DateModified]) VALUES (4, N'Formal Demand', 1, CAST(N'2020-02-17 11:38:38.290' AS DateTime))
+GO
+INSERT [dbo].[Actions] ([ActionID], [ActionName], [ActionStatus], [DateModified]) VALUES (5, N'High Net', 1, CAST(N'2020-02-17 11:38:38.290' AS DateTime))
+GO
+INSERT [dbo].[Actions] ([ActionID], [ActionName], [ActionStatus], [DateModified]) VALUES (7, N'Telephone', 1, CAST(N'2020-02-17 11:38:38.290' AS DateTime))
+GO
+INSERT [dbo].[Actions] ([ActionID], [ActionName], [ActionStatus], [DateModified]) VALUES (8, N'Lending Applications', 1, CAST(N'2020-06-03 22:17:01.923' AS DateTime))
+GO
+INSERT [dbo].[Actions] ([ActionID], [ActionName], [ActionStatus], [DateModified]) VALUES (9, N'I/Card', 1, CAST(N'2020-06-05 09:38:57.873' AS DateTime))
+GO
+INSERT [dbo].[Actions] ([ActionID], [ActionName], [ActionStatus], [DateModified]) VALUES (10, N'SMS Alert', 1, CAST(N'2020-06-11 18:22:02.980' AS DateTime))
+GO
+SET IDENTITY_INSERT [dbo].[Actions] OFF
+GO
